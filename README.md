6942dc3e8b908c3053911d9a03aa0c33e940315d
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-08-28 22:56:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AngularJS Simple Banker Application
---
+++ app/js/controllers.js
@@ -0,0 +1,51 @@
+var app = angular.module('bankerApp', ['xeditable']);
+
+app.controller('TransactionListController', function($scope) {
+  var transactionList = this;
+  transactionList.transactions = [
+    { text: 'Salary', amount: 1000, datetime: new Date(), checked: false },
+    { text: 'taxi', amount: 50, datetime: new Date(), checked: false }
+  ];
+
+  transactionList.addTransaction = function() {
+    var correctedAmount = transactionList.transactionAmount.replace(',', '.');
+    transactionList.transactions.push({
+      text: transactionList.transactionText,
+      amount: correctedAmount,
+      datetime: transactionList.transactionDate,
+      checked: false
+    });
+    transactionList.transactionText = '';
+    transactionList.transactionAmount = '';
+    transactionList.transactionDate = new Date();
+    document.getElementById('transaction_text').focus();
+  };
+
+  transactionList.archive = function() {
+    var oldTransactions = transactionList.transactions;
+    transactionList.transactions = [];
+    angular.forEach(oldTransactions, function(transaction) {
+      if (!transaction.checked) transactionList.transactions.push(transaction);
+    });
+  };
+
+  transactionList.balance = function() {
+    var sum = 0;
+    angular.forEach(transactionList.transactions, function(transaction) {
+      sum += parseFloat(transaction.amount);
+    });
+    return sum;
+  };
+
+  $scope.currentDate = new Date();
+
+  $scope.checkDate = function(str) {
+    if (!Date.parse(str)) {
+      return "Should be correct date!";
+    }
+  };
+});
+
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
