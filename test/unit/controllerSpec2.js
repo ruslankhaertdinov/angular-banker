0606a9d02b2706869212ee9b35cefde5ff0ee497
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-08-31 19:57:00 +0300

    disable legacy specs, add new controller spec with 1 expectation

---
+++ test/unit/controllerSpec2.js
@@ -0,0 +1,20 @@
+describe('TransactionListController', function() {
+  beforeEach(module('bankerApp'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.checkDate', function() {
+    it('returns message if argument is not date representation', function() {
+      var $scope = {};
+      var controller = $controller('TransactionListController', { $scope: $scope });
+      expect($scope.checkDate('not_date')).toEqual('Should be correct date!');
+
+      var correctDateStr = 'Mon Aug 31 2015 19:44:03 GMT+0300 (MSK)';
+      expect($scope.checkDate(correctDateStr)).toEqual(null);
+    });
+  });
+});
