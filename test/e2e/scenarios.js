4bd3d836638392d417ae7165be14ce87365ebc77
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-09-01 22:03:53 +0300

    add e2e tests: start

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,101 +1,39 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
-
-describe('PhoneCat App', function() {
-
-  it('should redirect index.html to index.html#/phones', function() {
+describe('simpleBanker App', function() {
+  beforeEach(function() {
     browser.get('app/index.html');
-    browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones');
-      });
   });
 
-
-  describe('Phone list view', function() {
-
-    beforeEach(function() {
-      browser.get('app/index.html#/phones');
-    });
-
-
+  describe('transactions list', function() {
     it('should filter the phone list as a user types into the search box', function() {
-
-      var phoneList = element.all(by.repeater('phone in phones'));
+      var transactions = element.all(by.repeater('transaction in transactionList.transactions'));
       var query = element(by.model('query'));
 
-      expect(phoneList.count()).toBe(20);
+      expect(transactions.count()).toBe(2);
 
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
+      query.sendKeys('taxi');
+      expect(transactions.count()).toBe(1);
 
       query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(8);
+      query.sendKeys('lottery');
+      expect(transactions.count()).toBe(0);
     });
 
+    it('should reorder when date is changed', function(){});
+  });
 
-    it('should be possible to control phone order via the drop down select box', function() {
+  describe('Transaction details', function() {
+    it('should display transaction details', function() {
+      expect(element(by.xpath('/html/body/div/div/div[2]/table/tbody/tr[1]/td[1]')).getText()).toBe('Salary');
+      expect(element(by.xpath('/html/body/div/div/div[2]/table/tbody/tr[1]/td[2]')).getText()).toBe('1000');
 
-      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
-      var query = element(by.model('query'));
-
-      function getNames() {
-        return phoneNameColumn.map(function(elm) {
-          return elm.getText();
-        });
-      }
-
-      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
-
-      expect(getNames()).toEqual([
-        "Motorola XOOM\u2122 with Wi-Fi",
-        "MOTOROLA XOOM\u2122"
-      ]);
-
-      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-      expect(getNames()).toEqual([
-        "MOTOROLA XOOM\u2122",
-        "Motorola XOOM\u2122 with Wi-Fi"
-      ]);
-    });
-
-
-    it('should render phone specific links', function() {
-      var query = element(by.model('query'));
-      query.sendKeys('nexus');
-      element.all(by.css('.phones li a')).first().click();
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones/nexus-s');
-      });
+      expect(element(by.xpath('/html/body/div/div/div[2]/table/tbody/tr[2]/td[1]')).getText()).toBe('taxi');
+      expect(element(by.xpath('/html/body/div/div/div[2]/table/tbody/tr[2]/td[2]')).getText()).toBe('50');
     });
   });
 
-
-  describe('Phone detail view', function() {
-
-    beforeEach(function() {
-      browser.get('app/index.html#/phones/nexus-s');
-    });
-
-
-    it('should display nexus-s page', function() {
-      expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
-    });
-
-
-    it('should display the first phone image as the main phone image', function() {
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-    });
-
-
-    it('should swap main image if a thumbnail image is clicked on', function() {
-      element(by.css('.phone-thumbs li:nth-child(3) img')).click();
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
-
-      element(by.css('.phone-thumbs li:nth-child(1) img')).click();
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-    });
-  });
+  it('should add new transaction when submit form', function(){});
+  it('should archive selected transaction when click on Archive button', function(){});
+  it('should show current balance', function(){});
 });
